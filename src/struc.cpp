f04cda31f739945964e09e5bcfc70b8ad3e5c6de
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-19 16:36:48 +0800

    struc2vec function

---
+++ src/struc.cpp
@@ -0,0 +1,17 @@
+#include "struc.h"
+
+void Struc2vec(string filename, int dimension, bool undirected, int num_walks, int walk_length){
+    Graph G(filename, undirected);
+    struc2vec model(G, true);
+
+    model.PreprocessNeighborsBFS();
+    model.CalDistVertices();
+    // model.CalDistAllVertices();
+    model.CreateDistNetwork();
+    model.PreprocessParamsRandomWalk();
+    vector< vector<long> > walks = model.SimulateWalks(num_walks, walk_length);
+
+    SaveRandomWalks(walks, G);
+    word2vec_train("random_walks.txt", "w2v.model", dimension);
+    SaveEmbedding("w2v.model", "embedding.emb", G);
+}
---
+++ src/struc.h
@@ -0,0 +1,8 @@
+#ifndef _STRUC_H_
+#define _STRUC_H_
+
+#include "struc2vec/struc2vec.h"
+
+void Struc2vec(string filename, int dimension=128, bool undirected=false, int num_walks=10, int walk_length=80);
+
+#endif
