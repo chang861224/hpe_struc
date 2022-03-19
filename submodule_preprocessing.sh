a8d1c7f56c139fc41f8f7ab9aadc24319d529482
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-19 17:03:22 +0800

    submodule preprocessing

---
+++ submodule_preprocessing.sh
@@ -0,0 +1,14 @@
+git submodule init
+git submodule update
+
+# smore compile
+cd src/smore
+make
+cd ../../
+
+# struc2vec get submodule and compile 
+cd src/struc2vec
+git submodule init
+git submodule update
+sh word2vec_compile.sh
+cd ../../
